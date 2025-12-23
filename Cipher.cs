1ef1198ffb42cd04aa30ed181fa386df7ce48823
0LIK-GIT <olikt9@gmail.com>
2025-12-23 20:34:59 +0100

    first commit v2

---
+++ Cipher.cs
@@ -0,0 +1,46 @@
+﻿using System;
+public class Cipher
+{
+    private string text;
+    private string[] results = new string[25];
+    public Cipher(string text)
+    {
+        this.text = text;
+    }
+
+    private void Decode()
+    {
+        for (int i = 0; i < 25; i++) // for each key
+        {
+            string new_result = "";
+            for (int j = 0; j < text.Length; j++) // for each letter
+            {
+                int character = text[j];
+                if ((character >= 65) && (character <= 90))
+                {
+                    character = character + i + 1;
+                    if (character > 90)
+                        character = character - 90 + 64;
+                }
+                else if ((character >= 97) && (character <= 122))
+                {
+                    character = character + i + 1;
+                    if (character > 122)
+                        character = character - 122 + 96;
+                }
+                new_result += (char)character;
+            }
+            results[i] = new_result;
+        }
+    }
+    
+    public void Results()
+    {
+        Decode();
+        int i = 1;
+        foreach (string s in results)
+        {
+            Console.WriteLine(i++ + ": " + s);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+public class Program
+{
+    static void Main()
+    {
+        string text;
+        Console.Write("Enter yout cipher: ");
+        text = Console.ReadLine();
+
+        Cipher cipher = new Cipher(text);
+
+        cipher.Results();
+    }
+}
